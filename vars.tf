054d9b63de020e9fd7e1594c800311c067c649cd
Sridhar Doniparti <sridhard29@yahoo.com>
2020-06-02 07:30:47 +0000

    New files

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+    description = "Your AWS Access Key"
+    default = "AKIAVQN6SLPJGGYT6ONN"
+}
+
+variable "secret_key" {
+    description = "Your AWS Secret Key"
+    default = "AKIAVQN6SLPJOGEUXIV2"
+}
